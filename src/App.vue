5e69a1be7d029fe868d032cf4aa25c3639b5cff1
Akhil P Jose <akhil.jose@vonnue.com>
2024-05-13 11:42:42 +0530

    Feat: add UI for testing

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,49 @@
-<script setup></script>
+<script setup>
+	import { ref } from "vue";
+
+	const appleCount = ref(0);
+	const newCategory = ref("default");
+</script>
 
 <template>
 	<div>
+		<p v-t="{ path: 'message.hi', args: { name: 'Akhil' } }"></p>
+		{{ newCategory }}
+		<select class="form-select form-control" v-model="newCategory">
+			<option value="default">Please Select</option>
+			<option value="en">en</option>
+			<option value="ja">ja</option>
+		</select>
 		<h1>Hello World</h1>
+		<p v-t="{ path: 'message.apple', plural: appleCount }"></p>
+		<div class="button-container">
+			<button
+				type="button"
+				@click="
+					() => {
+						appleCount++;
+					}
+				"
+			>
+				add apple
+			</button>
+			<button
+				type="button"
+				@click="
+					() => {
+						appleCount = 0;
+					}
+				"
+			>
+				reset apple count
+			</button>
+		</div>
 	</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+	.button-container {
+		display: flex;
+		gap: 20px;
+	}
+</style>
--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -3,8 +3,20 @@ import { createI18n } from 'vue-i18n'
 const i18n = createI18n({
     locale: 'en',
     messages: {
-        en: { hello: 'hi there!' },
-        ja: { hello: 'こんにちは！' }
+        en: {
+            message: {
+                hi: 'Hi, {name}!',
+                bye: 'good bye!',
+                apple: 'no apples | one apple | {count} apples'
+            }
+        },
+        ja: {
+            message: {
+                hi: 'こんにちは、 {name}！',
+                bye: 'さようなら！',
+                apple: 'リンゴはありません | 一つのりんご | {count} りんご'
+            }
+        }
     }
 })
 
