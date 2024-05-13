0702e80cf924c4bebed4708d8696591ce542cc33
Akhil P Jose <akhil.jose@vonnue.com>
2024-05-13 11:51:34 +0530

    Feat: change language selector

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
 	import { ref } from "vue";
 
 	const appleCount = ref(0);
-	const newCategory = ref("default");
 </script>
 
 <template>
 	<div>
 		<p v-t="{ path: 'message.hi', args: { name: 'Akhil' } }"></p>
-		{{ newCategory }}
-		<select class="form-select form-control" v-model="newCategory">
-			<option value="default">Please Select</option>
-			<option value="en">en</option>
-			<option value="ja">ja</option>
+		selected language: {{ $i18n.locale }}
+		<select v-model="$i18n.locale">
+			<option
+				v-for="locale in $i18n.availableLocales"
+				:key="`locale-${locale}`"
+				:value="locale"
+			>
+				{{ locale }}
+			</option>
 		</select>
 		<h1>Hello World</h1>
 		<p v-t="{ path: 'message.apple', plural: appleCount }"></p>
